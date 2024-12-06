5fe875e2c162615d04bb9cb34a778ed0930308fd
Yevhenii Pletenskyi <yevhenii.p@thewhitelabelagency.com>
2024-12-07 00:23:42 +0200

    bugs fixing

--- src/js/Game.js
+++ src/js/Game.js
@@ -118,41 +118,38 @@ export default class Game {
     }
 
     checkPlacementConditions(row, col, blockId) {
-        // Перевіряємо умови для кожної конкретної клітинки
         if ((row === 0 && col === 1 && blockId !== 4) ||
             (row === 0 && col === 7 && blockId !== 5) ||
             (row === 7 && col === 1 && blockId !== 6) ||
             (row === 7 && col === 7 && blockId !== 7)) {
-            return false; // Не можна поставити блок
+            return false;
         }
-        return true; // Блок можна поставити
+        return true;
     }
 
     checkAllBlocksInPlace() {
         const requiredPositions = [
-            { row: 0, col: 1, blockId: 4 },
-            { row: 0, col: 7, blockId: 5 },
-            { row: 7, col: 1, blockId: 6 },
-            { row: 7, col: 7, blockId: 7 }
+            {row: 0, col: 1, blockId: 4},
+            {row: 0, col: 7, blockId: 5},
+            {row: 7, col: 1, blockId: 6},
+            {row: 7, col: 7, blockId: 7}
         ];
 
-        for (const { row, col, blockId } of requiredPositions) {
+        for (const {row, col, blockId} of requiredPositions) {
             if (this.gameMatrix[row][col] !== blockId) {
-                return false; // Блок не на своєму місці
+                return false;
             }
         }
-        return true; // Усі блоки на своїх місцях
+        return true;
     }
 
     onPointerDown(event) {
         const localPos = event.data.getLocalPosition(this.cellContainer);
         this.initialMousePosition = {x: localPos.x, y: localPos.y};
 
-        // Отримуємо координати кліка відносно `cellContainer`
         const col = Math.floor(localPos.x / this.cellWidth);
         const row = Math.floor(localPos.y / this.cellHeight);
 
-        // Перевіряємо, чи координати в межах матриці
         if (
             row >= 0 &&
             row < this.gameMatrix.length &&
@@ -161,7 +158,6 @@ export default class Game {
         ) {
             const blockId = this.gameMatrix[row][col];
             if (blockId !== 1) {
-                // Знаходимо блок у cellContainer
                 const block = this.cellContainer.children.find(
                     (child) => {
                         return (
@@ -185,12 +181,8 @@ export default class Game {
     onBlockGrab(block) {
         this.draggedBlock = block;
 
-        // Зберігаємо координати миші та початкові координати блоку
-        // this.initialMousePosition = mousePosition;
         this.initialBlockPosition = {x: block.x, y: block.y};
 
-        // Додаємо обробник для pointermove та pointerup
-        // this.app.stage.on('pointermove', this.onPointerMoveHandler);
         this.app.stage.on('pointerup', this.onPointerUpHandler);
         this.app.ticker.add(this.updateBlockPosition, this);
     }
@@ -207,65 +199,37 @@ export default class Game {
         let directionX = deltaX > 0 ? 1 : deltaX < 0 ? -1 : 0;
         let directionY = deltaY > 0 ? 1 : deltaY < 0 ? -1 : 0;
 
-        // if (this.gameMatrix[row][col + directionX] !== 1) {
-        //     if (directionX > 0) {
-        //         col = Math.floor(this.draggedBlock.x / this.cellWidth);
-        //         row = Math.floor(this.draggedBlock.y / this.cellHeight);
-        //     } else if (directionX < 0) {
-        //         col = Math.ceil(this.draggedBlock.x / this.cellWidth);
-        //         row = Math.ceil(this.draggedBlock.y / this.cellHeight);
-        //     }
-        // }
-        //
-        // const nextRow = row + directionY;
-        // console.log('nextRow',nextRow)
-        // console.log(this.gameMatrix[nextRow][col])
-        // if (nextRow >= 0 && nextRow < this.gameMatrix.length && this.gameMatrix[nextRow][col] !== 1) {
-        //     if (directionY > 0) {
-        //         col = Math.ceil(this.draggedBlock.x / this.cellWidth);
-        //         row = Math.ceil(this.draggedBlock.y / this.cellHeight);
-        //     } else if (directionY < 0) {
-        //         col = Math.floor(this.draggedBlock.x / this.cellWidth);
-        //         row = Math.floor(this.draggedBlock.y / this.cellHeight);
-        //     }
-        // }
-
         if (Math.abs(deltaX) > Math.abs(deltaY) && this.gameMatrix[row][col + directionX] === 1) {
             this.draggedBlock.y = this.initialBlockPosition.y;
             this.draggedBlock.x = this.initialBlockPosition.x + deltaX;
-        }else if (Math.abs(deltaX) < Math.abs(deltaY)) {
-            // Ensure the row index is within bounds
+        } else if (Math.abs(deltaX) < Math.abs(deltaY)) {
             const nextRow = row + directionY;
             if (nextRow >= 0 && nextRow < this.gameMatrix.length && this.gameMatrix[nextRow][col] === 1) {
                 this.draggedBlock.x = this.initialBlockPosition.x;
                 this.draggedBlock.y = this.initialBlockPosition.y + deltaY;
             }
         }
-        // Якщо попередні значення не встановлені, ініціалізуємо їх
+
         if (this.previousCol === null || this.previousRow === null) {
             this.previousCol = col;
             this.previousRow = row;
         }
 
-        // Перевірка, чи блок не може бути переміщений, якщо він на своєму місці
         if (!this.checkPlacementConditions(row, col, this.draggedBlock.block.id)) {
-            // Якщо блок не може бути переміщений, повертаємо його на початкову позицію
             this.draggedBlock.x = this.initialBlockPosition.x;
             this.draggedBlock.y = this.initialBlockPosition.y;
             return;
         }
 
-        // Перевіряємо, чи змінилася колонка або рядок
         if (col !== this.previousCol || row !== this.previousRow) {
             this.updateMatrixAndPosition(this.previousRow, this.previousCol, row, col);
             this.previousCol = col;
             this.previousRow = row;
         }
+
     }
 
-// Допоміжний метод для оновлення матриці та позиції
     updateMatrixAndPosition(row, col, nextRow, nextCol) {
-        // Оновлюємо матрицю
         this.gameMatrix[row][col] = 1;
         this.gameMatrix[nextRow][nextCol] = this.draggedBlock.block.id;
 
@@ -274,15 +238,16 @@ export default class Game {
 
     onBlockDrop() {
         if (!this.draggedBlock) return;
-        console.log('drop')
 
         const col = Math.round(this.draggedBlock.x / this.cellWidth);
         const row = Math.round(this.draggedBlock.y / this.cellHeight);
-        console.log(row)
 
-        // Перевіряємо, чи блок може бути поставлений на новому місці
+        if (this.previousCol === null || this.previousRow === null) {
+            this.previousCol = col;
+            this.previousRow = row;
+        }
+
         if (!this.checkPlacementConditions(row, col, this.draggedBlock.block.id)) {
-            // Якщо блок не можна поставити, повертаємо його на початкову позицію
             this.draggedBlock.x = this.initialBlockPosition.x;
             this.draggedBlock.y = this.initialBlockPosition.y;
             return;
@@ -294,19 +259,17 @@ export default class Game {
         this.draggedBlock.block.col = col;
         this.draggedBlock.block.row = row;
 
-        console.log(row)
-
         const prevCol = Math.round(this.initialBlockPosition.x / this.cellWidth);
         const prevRow = Math.round(this.initialBlockPosition.y / this.cellHeight);
-        this.gameMatrix[prevRow][prevCol] = 1; // Відновлюємо попередню позицію
-        this.gameMatrix[this.previousRow][this.previousCol] = this.draggedBlock.block.id; // Встановлюємо нову позиціюЂЂЂ
-        // Перевірка, чи всі блоки на своїх місцях
+        this.gameMatrix[prevRow][prevCol] = 1;
+        this.gameMatrix[this.previousRow][this.previousCol] = this.draggedBlock.block.id;
+
         if (this.checkAllBlocksInPlace()) {
             this.createPopup("Level Complete!", () => {
                 this.loadPrevLevel();
             }, () => {
                 this.loadNextLevel();
-            },()=> {
+            }, () => {
                 window.close();
             });
         }
@@ -319,38 +282,32 @@ export default class Game {
     }
 
     clearCurrentLevel() {
-        // Видаляємо всі блоки з контейнера
         this.cellContainer.removeChildren();
-        this.blocks = []; // Очищуємо масив блоків
-        this.gameMatrix = this.createMatrix(); // Скидаємо матрицю
+        this.blocks = [];
+        this.gameMatrix = this.createMatrix();
     }
 
     loadNextLevel() {
-        this.clearCurrentLevel(); // Очищаємо поточний рівень
-        this.currentLevel++; // Переходимо до наступного рівня
+        this.clearCurrentLevel();
+        this.currentLevel++;
 
-        // Перевіряємо, чи є наступний рівень
         if (this.currentLevel < this.levels.length) {
-            this.populateLevel(this.currentLevel); // Заповнюємо новий рівень
+            this.populateLevel(this.currentLevel);
         } else {
             console.log('Вітаємо! Ви пройшли всі рівні!');
-            // Тут можна додати логіку для завершення гри або перезапуску
         }
     }
 
     loadPrevLevel() {
-        this.clearCurrentLevel(); // Очищаємо поточний рівень
-        this.currentLevel--; // Переходимо до наступного рівня
+        this.clearCurrentLevel();
+        this.currentLevel--;
 
-        // Перевіряємо, чи є наступний рівень
         if (this.currentLevel < this.levels.length) {
-            this.populateLevel(this.currentLevel); // Заповнюємо новий рівень
-        } else {
-            console.log('Вітаємо! Ви пройшли всі рівні!');
-            // Тут можна додати логіку для завершення гри або перезапуску
+            this.populateLevel(this.currentLevel);
         }
     }
-    createPopup(message, onPrevLevel, onNextLevel , onClose) {
+
+    createPopup(message, onPrevLevel, onNextLevel, onClose) {
         console.log('popup')
         const popupContainer = new Container();
 
@@ -390,7 +347,7 @@ export default class Game {
         text.y = this.app.screen.height / 2 - 20;
         popupContainer.addChild(text);
 
-        if (this.currentLevel != 0){
+        if (this.currentLevel != 0) {
             // Кнопка "Попередній рівень"
             const prevButton = new Graphics();
             prevButton.beginFill(0xff0000); // Червона кнопка
@@ -419,7 +376,7 @@ export default class Game {
             });
         }
 
-        if (this.currentLevel != this.levels.length - 1){
+        if (this.currentLevel != this.levels.length - 1) {
             // Кнопка "Наступний рівень"
             const nextButton = new Graphics();
             nextButton.beginFill(0x007bff); // Синя кнопка
@@ -449,7 +406,7 @@ export default class Game {
             });
         }
 
-        if (this.currentLevel === this.levels.length - 1){
+        if (this.currentLevel === this.levels.length - 1) {
             // Кнопка "Наступний рівень"
             const closeButton = new Graphics();
             closeButton.beginFill(0x007bff); // Синя кнопка
